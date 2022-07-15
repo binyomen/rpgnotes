8d3baa7aeff6933e736e84e15ff617e17e23fdb3
Ben Weedon <ben@weedon.email>
2022-07-14 21:54:01 -0700

    Add nav bar

--- modules/collect.js
+++ modules/collect.js
@@ -1,7 +1,11 @@
 module.exports = function() {
     return function(files, metalsmith, done) {
-        metalsmith.metadata().uncategorised = [];
-        metalsmith.metadata().uncategorised.metadata = {title: 'Uncategorised'};
+        const metadata = metalsmith.metadata();
+
+        metadata.collections.uncategorised = [];
+        metadata.collections.uncategorised.metadata = {
+            title: 'Uncategorised',
+        };
 
         for (const [path, file] of Object.entries(files)) {
             if (file.collection) {
@@ -10,11 +14,12 @@ module.exports = function() {
                 const newPath = file.collection + '/' + path;
                 files[newPath] = file;
             } else if (!file.isHome && file.title) {
-                metalsmith.metadata().uncategorised.push(file);
+                metadata.collections.uncategorised.push(file);
             }
         }
 
-        for (const [name, collection] of Object.entries(metalsmith.metadata().collections)) {
+        for (const [name, collection] of Object.entries(metadata.collections)) {
+            collection.metadata.path = '/' + name + '/';
             files[name + '.md'] = {
                 title: collection.metadata.title,
                 layout: 'collection_page.hbs',
